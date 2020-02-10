4a5d1be6fa94401b9eb7a36b3e03e41e03653231
Rahul Sanjay Avvaru <rahulsanjay@Rahuls-MacBook-Pro.local>
2020-02-10 12:51:47 -0700

    Renaming nlp.py to foil_parser.py

--- temp.py
+++ temp.py
@@ -1,4 +1,4 @@
-from nlp import get_actions
+from foil_parser import get_actions
 
 user_foil = "why deploy rescuers fire chief phoenix fire brickyard and address media transport chief instead of deploy small engines fire chief scotts fire lake and address media medi chief"
 why, why_not = get_actions(user_foil, current_plan=set())
