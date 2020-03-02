7964e6b9de047f55903f9e339d3e9058cfc9efa4
kvalmeek <karthikv792@gmail.com>
2020-03-02 16:22:11 -0700

    SPEECH FILE CORRECTION

--- speak.py
+++ speak.py
@@ -6,12 +6,12 @@ class Speak():
             'VALIDATE_PLAN'     : 'I validated the plan for you.  If the plan appears in green, you are good to go.  Otherwise, if you see a red action, hover over it to see the reason it is invalid.',
             'CORRECT_PLAN'      : 'I corrected the invalid plan.  It should work like a charm now!',
             'SUGGEST_PLAN'      : 'Here is a plan that will help you achieve your Goal.  I am secretly praying you like it.',
-            'EXPLAIN'           : 'I am sorry that the plan seemsed inexplicable.  Here is what I think might be the reason for that.',
+            'EXPLAIN'           : 'I am sorry that the plan seemed inexplicable.  Here is what I think might be the reason for that.',
             'RESOURCE_UPDATED'  : 'The resource was successfully updated.',
             'OPTIMAL_PLAN'      : 'This is the best plan in terms of action costs that achieves the goal.',
             'GOAL_SELECTED'     : 'Your goal was successfully selected.',
             'MODEL_UPDATED'     : 'Thank you for reviewing my explanations and providing valuable feedback.  I have updated both our models in accordance to your wishes',
-            'ADD_FOIL'          : 'Welcome! You can start adding actions to your foil by using the drop down list or use my latest speech interface.',
+            'ADD_FOIL'          : 'Have another idea for your plan? You can start by adding actions to your foil by using the drop down list or use my latest speech interface.',
             'NEAREST_PLAN'      : 'Here is a plan that is the closest to your foil. Hope this satisfies you.'
         }
 
