3e760184e58f96899f0b12f1e5ef2e7761bd9971
kvalmeek <karthikv792@gmail.com>
2020-05-28 19:26:58 -0700

    Plan preferences, Planners in parallel

--- planner/Preferences/get_plan.sh
+++ planner/Preferences/get_plan.sh
@@ -6,5 +6,5 @@ FD_PATH=$(locate fast-downward.py | head -n 1)
 # find optimal plan using fd on input domain and problem #
 rm -f output output.sas sas_plan
 #timeout 10s ${FD_PATH} $1 $2 --search "astar(lmcut())" | grep -e \([0-9]\) | awk '{$NF=""; print $0}'
-timeout 3s ${FD_PATH} $1 $2 --search "astar(hm(m=3))" > /dev/null #| grep  "Simplified to trivially false goal" #> /dev/null #| grep -e \([0-9]\) | awk '{$NF=""; print $0}'
+timeout 2s ${FD_PATH} $1 $2 --search "astar(hm(m=3))" > /dev/null #| grep  "Simplified to trivially false goal" #> /dev/null #| grep -e \([0-9]\) | awk '{$NF=""; print $0}'
 if [ $? -eq 124 ]; then echo -e "(dummy)\n; cost = 15 (general cost)"; echo -e "(dummy)\n; cost = 15 (general cost)" > sas_plan ; fi
