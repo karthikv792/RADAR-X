7d3fefdc8c630a8eccb520ee99c69d7c3a239342
kvalmeek <karthikv792@gmail.com>
2020-05-28 19:27:09 -0700

    Plan preferences, Planners in parallel

--- radar.py
+++ radar.py
@@ -1,39 +1,21 @@
-<<<<<<< HEAD
-from flask import Flask, render_template, request, jsonify, Response
-=======
 import re
 from flask import Flask, render_template, request, jsonify
->>>>>>> temp
 import json
-import requests
-import urllib
 from planner import Planner
 from speak import Speak
 from dbHandler import dbHandler
-<<<<<<< HEAD
-import speech_recognition as sr
-import io
-from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
-=======
 from foil_parser import get_actions
 import speech_recognition as sr
->>>>>>> temp
 app = Flask(__name__)
 planner = Planner()
-speech1 = Speak()
+speech = Speak()
 dbCaller = dbHandler()
 recognizer = sr.Recognizer()
-<<<<<<< HEAD
-#dbCaller.initializeDatabase()
-=======
 
 dbCaller.initializeDatabase()
->>>>>>> temp
 
 @app.route("/")
-def index(exp=0, s=speech1.getSpeechText('INTRO'),gs='Extinguish Big Fire at BYENG'):
+def index(exp=0, s=speech.getSpeechText('INTRO'),gs='Extinguish Big Fire at BYENG'):
     planner.definePlanningProblem(gs)
     a = planner.getActionNames()
     if planner.initial:
@@ -41,7 +23,7 @@ def index(exp=0, s=speech1.getSpeechText('INTRO'),gs='Extinguish Big Fire at BYE
     g = ['Extinguish Big Fire At Byeng']#, 'Extinguish Small Fire At Byeng']
     if not a:
         # o = planner.getExcuses()
-        # s = speech1.getSpeechText('INVALID_STATE')
+        # s = speech.getSpeechText('INVALID_STATE')
         # lm = []
         pass
     else:
@@ -81,7 +63,7 @@ def getPresentPlan(request):
 @app.route("/updateModels", methods=['GET','POST'])
 def updateModels():
     planner.reconcileModels( dict(request.form) )
-    return index(s=speech1.getSpeechText('MODEL_UPDATED'))
+    return index(s=speech.getSpeechText('MODEL_UPDATED'))
 
 @app.route("/updateGoals", methods=['GET','POST'])
 def updateGoals():
@@ -89,47 +71,18 @@ def updateGoals():
     print("=================")
     print(d['option'])
     print("=================")
-    return index(s=speech1.getSpeechText('GOAL_SELECTED'),gs=d['option'])
+    return index(s=speech.getSpeechText('GOAL_SELECTED'),gs=d['option'])
 
 @app.route("/generateAlternative", methods=['GET','POST'])
 def getOptimalPlan():
     planner.getSuggestedPlan({})
-    return index(s=speech1.getSpeechText('OPTIMAL_PLAN'))
-
-
-@app.route("/foil",methods=['GET','POST'])
-def foil():
-    plan = getPresentPlan(request)
-    acts = [x.strip('() \n') for x in plan.values()]
-    a = planner.getActionNames()
-    return render_template('foil.html',prePlan=acts,actions=a)
-
-@app.route("/foilrec",methods=['GET','POST'])
-def foilrec():
-    with open('service-account-file.json') as file:
-        GOOGLE_CREDENTIALS = file.read()
-    url = request.files['audio_data']
-    print("GOT DATA")
-
-    # url=url[5:]
-    # print(url)
-    # rawWav = requests.get(url,verify=False)
-
-    wavFile = sr.AudioFile(url)
-    with wavFile as source:
-        #recognizer.adjust_for_ambient_noise(source)
-        audio = recognizer.record(source)
-    text = recognizer.recognize_google_cloud(audio, credentials_json=GOOGLE_CREDENTIALS)
-    print(text)
-    return request.url
-
-
+    return index(s=speech.getSpeechText('OPTIMAL_PLAN'))
 
 @app.route("/validate", methods=['GET', 'POST'])
 def validate():
     planner.savePlan()
     planner.getValidatedPlan(getPresentPlan(request))
-    return index(s=speech1.getSpeechText('VALIDATE_PLAN'))
+    return index(s=speech.getSpeechText('VALIDATE_PLAN'))
 
 @app.route("/getPlanExplanation", methods=['GET', 'POST'])
 def getExplanationForPlan():
@@ -145,7 +98,7 @@ def getExplanationForFoil():
 def updateResources():
     d = dict(request.form)
     dbCaller.updateResourcesInTable( d['resourceName'], d['tableName'], d['rowId'], d['presentState'])
-    return index(s=speech1.getSpeechText('RESOURCE_UPDATED'))
+    return index(s=speech.getSpeechText('RESOURCE_UPDATED'))
 
 @app.route("/foil",methods=['GET','POST'])
 def foil(s=speech.getSpeechText('ADD_FOIL'),closestplanfound=0,pres_plan={}):
@@ -218,13 +171,13 @@ def acceptClosestPlan():
 def suggest():
     planner.savePlan()
     planner.getSuggestedPlan(getPresentPlan(request))
-    return index(1, speech1.getSpeechText('SUGGEST_PLAN'))
+    return index(1, speech.getSpeechText('SUGGEST_PLAN'))
 
 @app.route("/fix", methods=['GET', 'POST'])
 def fix():
     planner.savePlan()
     planner.getSuggestedPlan(getPresentPlan(request), True)
-    return index(s=speech1.getSpeechText('CORRECT_PLAN'))
+    return index(s=speech.getSpeechText('CORRECT_PLAN'))
 
 @app.route("/undo", methods=['GET', 'POST'])
 def undo():
@@ -254,11 +207,5 @@ def readPoliceStationResource():
     data = dbCaller.getPoliceStationData()
     data = dbCaller.getUIReadyData( data, 'police_stations' )
     return jsonify( {"data" : data} )
-
-
 if __name__ == '__main__':
-<<<<<<< HEAD
-    app.run(host='0.0.0.0', port=5080, ssl_context=('cert.pem','key.pem'))
-=======
     app.run(host='0.0.0.0', port=5081)
->>>>>>> temp
